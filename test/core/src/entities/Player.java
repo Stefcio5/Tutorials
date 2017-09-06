8d49c7ef120fd927cb2cabea7aafa8e4e9d02e6c
DESKTOP-0PM4NBR\Stefcio <tefcio5@gmail.com>
2017-09-06 18:46:35 +0200

    Added player asset

--- test/core/src/entities/Player.java
+++ test/core/src/entities/Player.java
@@ -11,14 +11,14 @@ import com.badlogic.gdx.scenes.scene2d.actions.Actions;
  */
 public class Player extends com.badlogic.gdx.scenes.scene2d.ui.Image {
 
-    private static final int WIDTH = 77;
-    private static final int HEIGHT = 152;
+    private static final int WIDTH = 189;
+    private static final int HEIGHT = 300;
 
     private static final int STARTING_X = 200;
     private static final int STARTING_Y = 300;
 
     public Player() {
-        super(new Texture("badlogic.jpg"));
+        super(new Texture("Player.png"));
 
         this.setOrigin(WIDTH / 2, HEIGHT / 2);
         this.setSize(WIDTH, HEIGHT);
